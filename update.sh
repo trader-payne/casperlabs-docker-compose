92d8b88103a8fdaeef7b8d56282d3852d37e2ee0
trader-payne <baboialex95@gmail.com>
2020-11-08 13:25:48 +0100

    fixed update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ git pull
 echo "[network] 
 public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" | crudini --merge etc/casper/config.toml
 echo "[node] 
-trusted_hash='0e41a5edf2e2abb4f82fe829e939f16f3da70022c0ef56e50ecaadb9fbb237f8'" | crudini --merge etc/casper/config.toml
+trusted_hash='57c5101a76a7eefd6031b2d79b52b970fb0bb99f3aa1d0c15af373e571a8a386'" | crudini --merge etc/casper/config.toml
 
 docker-compose up -d --build --force-recreate
